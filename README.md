67a1c471ab268864c48f2f600e0b2da70a72b7f8
Daniël Heimans <dtm.dju@gmail.com>
2012-04-24 19:54:52 +0200

    Add script, update readme

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 deluge-to-sickbeard
 ============
 
-Tool to link your downloaded torrents from deluge to sickbeard, to process in such a way that you can keep seeding.
+Tool written in Python to link your downloaded torrents from deluge to sickbeard, 
+to process in such a way that you can keep seeding.
+
+So far only tested under linux with the same partition for the processing folder and the seeding folder.
+The advantage of this is that you can use hard links which allow you to keep seeding the file as well as have it 
+in your library. Using hard links also makes it possible to delete either the seeding file or the file in your lib
+while not losing both. The third and foremost advantage is that using hardlinks is that you can have a copy that
+you can rename while not storing the file twice, saving a lot of space!
+
+The idea is that deluge-to-sb.py is called by deluge upon completion, using the execute-plugin. 
+This will call the script with three arguments:
+
+- The torrent ID (we discard this)
+- The torrent name (identical to the filename if it's a single file, identical to the folder if it's a folder)
+- The folder where the torrent is stored
+
+This script works best if you have deluge setup to have labels for your series trackers (I use it with BTN)
+and tell deluge to move those torrents to a specific folder. In this case you can have a folder, say 
+/some/path/to/btn where deluge moves all your completed btn torrents. Since 'btn' is the lowest folder in that path
+and 'btn' is in the 'trackers' list, the script will execute for btn-torrents - and only those.
+
+
---
+++ deluge-to-sb.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+args = sys.argv
+
+torrentid = args[0]
+torrentname = args[1]
+torrentrootpath = args[2]
+
+
+# These file types will not be linked
+excluded_extensions = ['.png', '.nfo', '.jpg', '.txt']
+
+# These prefixed will be removed from the link, the link will still be made though!
+unwanted_prefixes = ['aaf-']
+
+# These dirs are skipped
+unwanted_dirs = ['Sample']
+
+# Needs to exist, needs to be on the same partition as the torrentfolder!
+destinationfolder = '/example/path/without/trailing/slash'
+
+# Only use for torrent folders ending in one of these folders:
+trackers = ['btn']
+
+
+####### - Do stuff
+
+def remove_prefix(name):
+    for prefix in unwanted_prefixes:
+        if name.startswith(prefix):
+            return name.replace(prefix,'',1)
+    return name
+
+
+# Only do stuff if the torrent is saved to one of the completed folders we want
+if os.path.split(torrentrootpath)[1] in trackers:
+    torrentpath = os.path.join(torrentrootpath, torrentname)
+
+    if os.path.isdir(torrentpath):
+        for root, dirs, files in os.walk(torrentpath):
+            for unwanted in unwanted_dirs:
+                if unwanted in dirs:
+                    dirs.remove(unwanted)
+            for name in files:
+                ext = os.path.splitext(name)[1]
+                if ext not in excluded_extensions:
+                    sourcepath = os.path.join(root,name)
+
+                    name = remove_prefix(name)
+
+                    destinationpath = os.path.join(destinationfolder, name)
+
+                    os.link(sourcepath, destinationpath)
+    else:
+        torrentname = remove_prefix(torrentname)
+        destinationpath = os.path.join(destinationfolder, torrentname)
+        os.link(torrentpath, destinationpath)
+
+
+        
+
+
